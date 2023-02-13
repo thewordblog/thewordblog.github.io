5714a79d0444e864e78d8ba5ca4bf5b57beeb5f0
TheWord Blog <blogoftheword@gmail.com>
2023-02-13 16:06:39 +0300

    Removing previous search bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,9 +19,6 @@ window.addEventListener('scroll', scrollHeader);
 
 // Open menu & search pop-up
 const menuToggleIcon = selectElement('#menu-toggle-icon');
-const formOpenBtn = selectElement('#search-icon');
-const formCloseBtn = selectElement('#form-close-btn');
-const searchContainer = selectElement('#search-form-container');
 
 const toggleMenu = () =>{
     const mobileMenu = selectElement('#menu');
@@ -31,13 +28,6 @@ const toggleMenu = () =>{
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 
-// Open/Close search form popup
-formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
-formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
-// -- Close the search form popup on ESC keypress
-window.addEventListener('keyup', (event) => {
-    if(event.key === 'Escape') searchContainer.classList.remove('activated');
-});
 
 // Switch theme/add to local storage
 const body = document.body;
