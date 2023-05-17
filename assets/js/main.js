0768928bd73e611e73a40be89e638467bbf51853
TheWord Blog <blogoftheword@gmail.com>
2023-05-17 14:00:34 +0300

    SEO management

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ const body = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 
-// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
+// Check to see if there is a theme preference in local Storage, if so add the light theme to the body
 if (currentTheme) {
     body.classList.add('light-theme');
 }
